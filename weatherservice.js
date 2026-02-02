df892c2ddff1fc9fba945e61e61cb92fb5d51d70
Janay Patel <121764581+YoursTruly1290@users.noreply.github.com>
2026-02-02 10:28:29 -0600

    Add static method getSummary to WeatherService

--- weatherservice.js
+++ weatherservice.js
@@ -11,7 +11,17 @@ class WeatherService {
       return new WeatherForecast(date, temp, summary);
     }
   
-    s
+    static getSummary(temp) {
+      if (temp > 40) {
+        return "Scorching";
+      } else if (temp > 30) {
+        return "Hot";
+      } else if (temp > 20) {
+        return "Warm";
+      } else if (temp > 10) {
+        return "Mild";
+      } else if (temp > 0) {
+        return "Cool";
       } else if (temp > -10) {
         return "Chilly";
       } else if (temp > -20) {
@@ -28,13 +38,14 @@ class WeatherService {
   
   class WeatherForecast {
     constructor(date, temperatureC, summary) {
-      
+      this.date = date;
       this.temperatureC = temperatureC;
       this.summary = summary;
     }
   
     get temperatureF() {
-
+      return 32 + Math.round(this.temperatureC / 0.5556);
+    }
 }
 
 module.exports = { WeatherService, WeatherForecast };
